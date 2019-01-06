714ed94bb5daf42e8c68d65fa058c6a9a17b9ffc
stefan Haslinger <mail@stefan-haslinger.at>
2019-01-06 15:41:43 +0100

    antenne kärnten

--- radio.py
+++ radio.py
@@ -70,14 +70,11 @@ while True:
   elif c == 195: # Umlauts
     c = stdscr.getch()
     if c == 188: # ü
-      tune_in('dlf', 
-              '-playlist https://www.deutschlandradio.de/streaming/dlf.m3u')
+      tune_in('dlf', '-playlist https://www.deutschlandradio.de/streaming/dlf.m3u')
     if c == 182: # ö
-      tune_in('dw', 
-              'http://dw-radio-german-mp3.akacast.akamaistream.net/7/506/135361/v1/gnl.akacast.akamaistream.net/dw-radio-german-mp3')
+      tune_in('radio_tirol',  'http://dw-radio-german-mp3.akacast.akamaistream.net/7/506/135361/v1/gnl.akacast.akamaistream.net/dw-radio-german-mp3')
     if c == 164: # ä
-      tune_in('hr1', 
-              'http://hr-hr1-live.cast.addradio.de/hr/hr1/live/mp3/128/stream.mp3')
+      tune_in('hr1', 'http://hr-hr1-live.cast.addradio.de/hr/hr1/live/mp3/128/stream.mp3')
   elif c == ord('p') or c == ord('P'):
     tune_in('hr2', 
             'http://hr-hr2-live.cast.addradio.de/hr/hr2/live/mp3/128/stream.mp3')
