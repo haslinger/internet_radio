30c515610d50c41186756022a4921fac03857790
Stefan Haslinger <mail@stefan-haslinger.at>
2021-06-29 17:10:30 +0200

    increase buffer size

--- radio.py
+++ radio.py
@@ -14,7 +14,7 @@ def tune_in(text, url):
   play(url)
 
 def play(url):
-  os.system('mplayer -really-quiet -nolirc -ao alsa -cache 128 ' + url + ' < /dev/null > /dev/null &')
+  os.system('mplayer -really-quiet -nolirc -ao alsa -cache 1024 ' + url + ' < /dev/null > /dev/null &')
 
 def ansage(text):
   play('~/internet_radio/ansagen/' + text + '.mp3')
